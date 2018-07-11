9645500f32551d56c4e24bf1db5b3f248e42b914
Albert Jennifer <35313442+HAJennifer@users.noreply.github.com>
2018-07-11 17:16:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-var countDownDate = new Date("Aug 18, 2018 15:37:25").getTime();
-var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("demo").innerHTML = days + ":" + hours + ":"
-  + minutes + ":" + seconds ;
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+
+$(document).ready(function() {
+
+  var nav = $("#nav-bar");
+
+  $(window).scroll(function() {
+    var originalHeight = nav.outerHeight(true);
+    var scrollTopVal = $(this).scrollTop();
+    
+    if (scrollTopVal > originalHeight + 130) {
+      nav.css("position", "fixed");
+      nav.css("top", "0");
+    } 
+  })
+
+});
