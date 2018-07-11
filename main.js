cc0e331b52ffc9e353f82e4478a25452e3fc05a8
Albert Jennifer <35313442+HAJennifer@users.noreply.github.com>
2018-07-11 21:44:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -15,20 +15,3 @@ $(document).ready(function() {
  
 
 });
-function isSafari() {
-    var ua = window.navigator.userAgent;
-    var iOS = !!ua.match(/iP(ad|od|hone)/i);
-    var hasSafariInUa = !!ua.match(/Safari/i);
-    var noOtherBrowsersInUa = !ua.match(/Chrome|CriOS|OPiOS|mercury|FxiOS|Firefox/i)
-    var result = false;
-    if(iOS) { //detecting Safari in IOS mobile browsers
-        var webkit = !!ua.match(/WebKit/i);
-        result = webkit && hasSafariInUa && noOtherBrowsersInUa
-    } else if(window.safari !== undefined){ //detecting Safari in Desktop Browsers
-        result = true;
-    } else { // detecting Safari in other platforms
-        result = hasSafariInUa && noOtherBrowsersInUa
-    }
-    return result;
-}
-
