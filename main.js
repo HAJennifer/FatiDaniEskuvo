38e42475aa7e76a37d1d7cb06127d774def94a89
Albert Jennifer <35313442+HAJennifer@users.noreply.github.com>
2018-07-11 17:35:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,15 +12,8 @@ $(document).ready(function() {
    
     }
   })
-  // once nav gets to top of page
-  // change position to fixed css
-  // once it gets back to where it originally was
-  // change position to relative
+ 
 
 });
 
-//$(document).ready(function(){
-    //$('#nav-menu').click(function(){
-       // $('ul.nav-list').addClass('open').slideToggle('50');
-   // });
-//});
+
