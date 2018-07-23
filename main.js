90f14f395b9fb6a208e29eaca963a247df1f79d9
Albert Jennifer <35313442+HAJennifer@users.noreply.github.com>
2018-07-23 19:05:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-$(document).ready(function() {
 
-  var nav = $("#nav-bar");
-
-  $(window).scroll(function() {
-    var originalHeight = nav.outerHeight(true);
-    var scrollTopVal = $(this).scrollTop();
-    
-    if (scrollTopVal > originalHeight + 130) {
-      nav.css("position", "fixed");
-      nav.css("top", "0");
-   
-    }
-  })
- 
-
-});
